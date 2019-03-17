978cfd67f4ae624b0ffa261ef6851457be34c6ce
Johnny Miller <johnnysviva@outlook.com>
2019-03-17 23:17:16 +0800

    fix($PinnedTagView): Pinned tag view disappeared after refresh page
    Closes #nvpea

--- src/permission.js
+++ src/permission.js
@@ -18,6 +18,13 @@ router.beforeEach((to, from, next) => {
       if (store.getters.roles.length === 0) {
         store.dispatch('GetInfo').then(res => { // 拉取用户信息
           next()
+          // 拉取user_info
+          const roles = res.data.roles // note: roles must be a object array! such as: [{id: '1', name: 'editor'}, {id: '2', name: 'developer'}]
+          store.dispatch('GenerateRoutes', { roles }).then(accessRoutes => {
+            // 根据roles权限生成可访问的路由表
+            router.addRoutes(accessRoutes) // 动态添加可访问路由表
+            next({ ...to, replace: true }) // hack方法 确保addRoutes已完成 ,set the replace: true so the navigation will not leave a history record
+          })
         }).catch((err) => {
           store.dispatch('FedLogOut').then(() => {
             Message.error(err || 'Verification failed, please login again')
